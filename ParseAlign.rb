896dd6686eff3ee4605bb919e98fcbb1f921c947
tkwsm <kawashima38@gmail.com>
2014-08-02 20:55:28 +0900

    create the first module as ParseAlign

--- ParseAlign.rb
+++ ParseAlign.rb
@@ -4,45 +4,53 @@ require 'rubygems'
 require 'bio'
 include Bio
 
-class ParseAlign
 
-  def initialize( consensus_hash )
-    @c = consensus_hash
-    @a = get_aligned
-  end
+module ParseAlign
 
-  attr_reader :c, :a
+  class HandleAlignmentHash
 
-  def get_aligned
-    aligned = {}
-    @c.each_key do |k|
-      if @c[k].include?("-")
-      aligned[k] = @c[k]
+    def initialize( consensus_hash )
+      @c = consensus_hash
+      @a = get_aligned
     end
-    return aligned
-  end
 
-  def get_all_conserved
-    @c.each_key do |k|
+    attr_reader :c, :a
+
+    def get_aligned
+      aligned = {}
+      @c.each_key do |k|
+        if @c[k].include?("-")
+          aligned[k] = @c[k]
+        end
+      end
+      return aligned
     end
+
+    def get_all_conserved
+      @c.each_key do |k|
+      end
+    end
+
+  end ## End of Create Alignment Hash
+
+  def CreateAlignmentHash( alignment_file_in_pir_format )
+
+    ch = {}; h  = {}; defline = "";
+    ff = FlatFile.new(FastaFormat, open(alignment_file_in_pir_format)
+    ff.each do |e|
+      defline = e.definition.slice(/\S+\;(\S+)/, 1)
+      h[ defline ] = e.naseq.split("")
+    end
+    ## Check #########
+    if h.values.collect{|v| v.size }.uniq.size > 1
+      print "The size of each sequence in the alignment is not same\n"; exit;
+    end
+    ################
+    return h
+ 
   end
 
 end
-
-ch = {}
-h  = {}
-ff = FlatFile.new(FastaFormat, ARGF)
-defline = ""
-ff.each do |e|
-  defline = e.definition.slice(/\S+\;(\S+)/, 1)
-  h[ defline ] = e.naseq.split("")
-end
-
-### Check #########
-  if h.values.collect{|v| v.size }.uniq.size > 1
-    print "The size of each sequence in the alignment is not same\n"; exit;
-  end
-#################
 
 alignment_size = h.values[0].size
 for i in 0..(alignment_size - 1)
@@ -53,6 +61,7 @@ for i in 0..(alignment_size - 1)
 end
 
 pa = ParseAlign.new( ch )
+p pa
 
 
 
