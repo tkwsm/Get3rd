614dc5e64bee8ea9f78b527956cad936ca2bce97
tkwsm <kawashima38@gmail.com>
2014-08-10 10:38:49 +0900

    again add test_ParseAlign.rb

--- test_ParseAlign.rb
+++ test_ParseAlign.rb
@@ -37,10 +37,9 @@ class Test_ParseAlign < MiniTest::Test
     assert_equal( Hash, @ha.get_compensation.class )
     assert_equal( nil, @ha.get_compensation[1][0] )
     assert_equal( nil, @ha.get_compensation[1][0] )
-    assert_equal( 0, @ha.get_compensation[1][8] )
-    assert_equal( nil, @ha.get_compensation[1][10] )
+    assert_equal( 1, @ha.get_compensation[1][8] )
     assert_equal( nil, @ha.get_compensation[563][0] )
-    assert_equal( 480, @ha.get_compensation[563][10] )
+    assert_equal( 482, @ha.get_compensation[564][10] )
   end
 
   def test_get_aligned
@@ -52,19 +51,24 @@ class Test_ParseAlign < MiniTest::Test
     assert_equal( Hash, @ha.get_all_conserved.class )
     assert_equal( 11, @ha.get_all_conserved.values[0].size )
     assert_equal( ["M"], @ha.get_all_conserved.values[0].uniq )
-    assert_equal( ["L"], @ha.get_all_conserved[367].uniq )
+    assert_equal( ["L"], @ha.get_all_conserved[366].uniq )
   end
 
   def test_check_correspondence
     assert_equal( true, @tc.check_correspondence )
   end
 
-  def test_show_corresponding_triplet
+  def test_show_corresponding_triplet_by_protein_position
     gid = "ENSMUSP00000027859"
-    pos_aa = 328
-    assert_equal( ["Y"], @ha.get_all_conserved[328].uniq )
-    assert_equal(Bio::Sequence::NA, @tc.show_corresponding_triplet(gid, pos_aa).class)
-    assert_equal("tac", @tc.show_corresponding_triplet(gid, pos_aa))
+    pos_aa = 90
+    assert_equal(Bio::Sequence::NA, @tc.show_corresponding_triplet_by_protein_position(gid, pos_aa).class)
+    assert_equal("atg", @tc.show_corresponding_triplet_by_protein_position(gid, pos_aa))
+    pos_aa = 91
+    assert_equal("ata", @tc.show_corresponding_triplet_by_protein_position(gid, pos_aa))
+    pos_aa = 92
+    assert_equal("gtg", @tc.show_corresponding_triplet_by_protein_position(gid, pos_aa))
+    pos_aa = 93
+    assert_equal("aca", @tc.show_corresponding_triplet_by_protein_position(gid, pos_aa))
   end
 
 end
